cd7dccbee1a627c89ac1f1db90770459028e45ba
alai06 <119883173+alai06@users.noreply.github.com>
2025-01-09 19:37:21 +0100

    Update Int_int_char.java

--- src/Int_int_char.java
+++ src/Int_int_char.java
@@ -1,4 +1,7 @@
 public class Int_int_char {
+    /* Cette classe nous permet gérer les trous (int_char), ainsi que la valeur evalué à partir de ce
+    coup joué (voir la fonction alpha_beta).
+     */
     int a;
     Int_char b;
     Int_int_char(int a, Int_char b) {
