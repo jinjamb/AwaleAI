c187171d0e99b7e4a4c62fcbb754ac9570a61664
alai06 <119883173+alai06@users.noreply.github.com>
2025-01-09 19:36:56 +0100

    Update Int_char.java

--- src/Int_char.java
+++ src/Int_char.java
@@ -1,4 +1,7 @@
 public class Int_char {
+    /* Cette classe nous permet de faire gérer les trous et les couleurs.
+    Par exemple, "3r" donnera new Int_char(3,"r")
+     */
     public int i;
     public char c;
     Int_char(int i, char c){
